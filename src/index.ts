5d3c7ee43a526c4f766907b41b030b1a9f4e9f0f
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-06-26 12:24:45 +0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata'
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up;
+//   creating a post in DB
+//   const post = orm.em.fork({}).create(Post, {
+//     title: "my first post",
+//   });
+//   await orm.em.persistAndFlush(post);
+
+const app = express()
+
+const apolloServer = new ApolloServer({
+  schema: await buildSchema({
+    resolvers: [PostResolver],
+    validate: false,
+  }),
+    //exporting orm.em to all resolvers
+  context:()=>({em:orm.em}),
+});
+
+
+
+apolloServer.applyMiddleware({app})
+
+app.listen(4000,()=>{
+  console.log("its running")
+})
+
+  const posts = await orm.em.find(Post,{});
+  console.log(posts)
+};
+
+main().catch((err) => {
+  console.error(err);
+});
